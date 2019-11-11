9035a97194a2275d645a9d996fd5ee6eee354169
Jim Thomason <jim@japanesewartuba.com>
2019-11-10 19:19:46 -0600

    Updates

---
+++ precache-manifest.63ec00cd7d185f59640a10c06fa9b319.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404794463b9b6d51168238464e714d95",
+    "url": "/class-spinner/index.html"
+  },
+  {
+    "revision": "13f4efa0fff84d0dd1a9",
+    "url": "/class-spinner/static/css/2.25279f69.chunk.css"
+  },
+  {
+    "revision": "3f1dce625862a142392f",
+    "url": "/class-spinner/static/css/main.7075be56.chunk.css"
+  },
+  {
+    "revision": "13f4efa0fff84d0dd1a9",
+    "url": "/class-spinner/static/js/2.8e5df26f.chunk.js"
+  },
+  {
+    "revision": "3f1dce625862a142392f",
+    "url": "/class-spinner/static/js/main.f8b69527.chunk.js"
+  },
+  {
+    "revision": "714263ee5f62d1d98e86",
+    "url": "/class-spinner/static/js/runtime-main.b94ddd84.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/class-spinner/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/class-spinner/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/class-spinner/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/class-spinner/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  }
+]);
